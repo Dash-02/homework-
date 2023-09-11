fb96c2e035ff6f83d7975a787401654cf9673486
Dash <dasha.semivolos@gmail.com>
2023-09-11 18:14:46 +0300

    c-p-p

---
+++ c-p-p/ametseidlaiev1/ametseidlaiev1/ametseidlaiev1.cpp
@@ -0,0 +1,27 @@
+﻿// ametseidlaiev1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int R;
+    cout << "Введите радиус основания: ";
+    cin >> R;
+
+    int h;
+    cout << "Введите высоту цилиндра: ";
+    cin >> h;
+
+    float v = M_PI * R*R*h;
+    cout << "Объем цилиндра: " << v << endl;
+    
+    return 0;
+}
+
