4634fd8c897c7330f5a693573b26135214c52bc0
Dash <dasha.semivolos@gmail.com>
2023-10-24 22:08:48 +0300

    user_names

---
+++ mdk_01.01/Project/Project/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя слитно, используя заглавные буквы: ");
+            string userName = Console.ReadLine();
+
+            string resultName = "";
+
+            for (int i = 0; i < userName.Length; i++)
+            {
+                if (Convert.ToString(userName[i]).ToUpper() == Convert.ToString(userName[i]))
+                {
+                    resultName += $" {userName[i]}";
+                }
+                else
+                {
+                    resultName += userName[i];
+                }
+            }
+
+            Console.WriteLine(resultName.Remove(0, 1));
+        }
+    }
+}
