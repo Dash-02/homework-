59764268c4270e3bc847ae6c0a3205ef9cffb1d3
Dash <dasha.semivolos@gmail.com>
2023-12-03 19:56:58 +0300

    course_project_1.0

---
+++ CURSACH/D/Production/Authorization.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Production
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.textBoxPas = new System.Windows.Forms.TextBox();
+            this.btn_enter = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxLog.Location = new System.Drawing.Point(65, 85);
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(159, 20);
+            this.textBoxLog.TabIndex = 2;
+            // 
+            // textBoxPas
+            // 
+            this.textBoxPas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPas.Location = new System.Drawing.Point(65, 153);
+            this.textBoxPas.Name = "textBoxPas";
+            this.textBoxPas.PasswordChar = '*';
+            this.textBoxPas.Size = new System.Drawing.Size(159, 20);
+            this.textBoxPas.TabIndex = 3;
+            // 
+            // btn_enter
+            // 
+            this.btn_enter.BackColor = System.Drawing.Color.Beige;
+            this.btn_enter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_enter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_enter.Location = new System.Drawing.Point(98, 194);
+            this.btn_enter.Name = "btn_enter";
+            this.btn_enter.Size = new System.Drawing.Size(87, 27);
+            this.btn_enter.TabIndex = 4;
+            this.btn_enter.Text = "Вход";
+            this.btn_enter.UseVisualStyleBackColor = false;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Beige;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_exit.Location = new System.Drawing.Point(98, 237);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(87, 27);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "Выйти";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(288, 363);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_enter);
+            this.Controls.Add(this.textBoxPas);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Authorization";
+            this.Text = "Авторизация";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.TextBox textBoxPas;
+        private System.Windows.Forms.Button btn_enter;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
