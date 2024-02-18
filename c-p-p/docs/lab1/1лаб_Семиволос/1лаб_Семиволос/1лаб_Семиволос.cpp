beb45c5b64252b7195a3fb3b554caa6347b47b13
Dash-02 <dasha.semivolos@gmail.com>
2024-02-18 20:03:54 +0300

    lab1-c-p-p

---
+++ c-p-p/docs/lab1/1лаб_Семиволос/1лаб_Семиволос/1лаб_Семиволос.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+//task1-24
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    float kg;
+    cout << "Введите количество килограммов: ";
+    cin >> kg;
+
+    float pudy = kg / 16.3807;
+    float funty = kg / 0.4095;
+    float loty = kg / 0.01638;
+    float zolotniki = kg / 0.002066;
+
+    cout << "Пуды: " << pudy << endl;
+    cout << "Фунты: " << funty << endl;
+    cout << "Лоты: " << loty << endl;
+    cout << "Золотники: " << zolotniki << endl;
+
+    return 0;
+}
---
+++ c-p-p/docs/lab1/лаб1_Сейдалиев/лаб1_Сейдалиев/лаб1_Сейдалиев.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+//task 1-23
+//int main() {
+//    setlocale(LC_ALL, "Ru");
+//
+//    int sm;
+//    cout << "Введите количество сантиметров: ";
+//    cin >> sm;
+//
+//    // Количество вершков в сантиметре
+//    float vershok = 4.4445;
+//    // Количество футов в сантиметре
+//    float fut = 30.48;
+//    // Количество дюймов в сантиметре
+//    float dum = 2.54;
+//
+//    // Перевод сантиметров в вершки
+//    float SMvershok = sm / vershok;
+//    // Перевод сантиметров в футы
+//    float SMfut = sm / fut;
+//    // Перевод сантиметров в дюймы
+//    float SMdum = sm / dum;
+//
+//    // Вывод результатов
+//    cout << "Вершки: " << SMvershok << endl;
+//    cout << "Футы: " << SMfut << endl;
+//    cout << "Дюймы: " << SMdum << endl;
+//
+//    return 0;
+//}
+
+//task 1-17
+int main() {
+    setlocale(LC_ALL, "Ru");
+    float r1, r2, r;
+    int t;
+
+    cout << "вычисление сопротивления электрической цепи" << endl;
+    cout << "величина первого сопротивления: ";
+    cin >> r1;
+    cout << "величина второго сопротивления: ";
+    cin >> r2;
+    cout << "тип соединения: последовательное " << endl;
+    r = (r1 * r2) / (r1 + r2);
+
+    cout << "сопротивление равно " << r << " Ом" << endl;
+
+    return 0;
+}
