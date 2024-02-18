9c9ba0e2ac2d45742b99fec4af18424de76749f9
Dash-02 <dasha.semivolos@gmail.com>
2024-02-18 21:03:10 +0300

    lab3-lab4-c-p-p

---
+++ c-p-p/docs/lab3/лаб3_Семиволос/лаб3_Семиволос/лаб3_Семиволос.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    cout << " x    |    y" << endl;
+    cout << "-------------" << endl;
+
+    for (float x = -2; x <= 2; x += 0.5) {
+
+        float y = -2.4 * x * x + 5 * x - 3;
+        cout << fixed << setprecision(2) << setw(5) << x << " | " << setw(7) << y << endl;
+    }
+
+    return 0;
+}
---
+++ c-p-p/docs/lab5/пр_5/пр_5.cpp
@@ -0,0 +1,157 @@
+﻿//#include <iostream>
+//#include <cmath>
+//#include <algorithm>
+//using namespace std;
+//
+//int main() {
+//    setlocale(LC_ALL, "Ru");
+//
+//    int n;
+//    float a, b;
+//    cout << "Введите длину массива: ";
+//    cin >> n;
+//
+//    const int maxSize = 100;
+//    float arr[maxSize];
+//    float sumModule = 0;
+//
+//    for (int i = 0; i < n; i++) {
+//        cout << "Элемент " << i + 1 << ": ";
+//        cin >> arr[i];
+//    }
+//
+//    cout << "Введенный массив: ";
+//    for (int i = 0; i < n; i++) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    cout << "Введите интервал [a, b], a: ";
+//    cin >> a;
+//    cout << "b: ";
+//    cin >> b;
+//
+//    float count = 0;
+//    for (int i = 0; i < n; i++) {
+//        if (arr[i] >= a && arr[i] <= b) {
+//            count++;
+//        }
+//    }
+//    cout << "Количество элементов в диапазоне от " << a << " до " << b << ": " << count << endl;
+//    
+//    float maxElement = arr[0]; 
+//    int maxIndex = 0; 
+//    float sumModulus = 0; 
+//    for (int i = 0; i < n; i++) {
+//        if (arr[i] > maxElement) {
+//            maxElement = arr[i];
+//            maxIndex = i;
+//        }
+//        else if (i > maxIndex) { 
+//            sumModulus += fabs(arr[i]);
+//        }
+//    }
+//    cout << "Сумма модулей элементов после максимального элемента: " << sumModulus << endl;
+//    
+//    sort(arr, arr + n, [](float a, float b) {
+//        return fabs(a) > fabs(b);
+//        });
+//
+//    cout << "Отсортированный массив: ";
+//    for (int i = 0; i < n; i++) {
+//        cout << arr[i] << " ";
+//    }
+//    
+//    return 0;
+//}
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    int n;
+    cout << "Введите длину массива: "; 
+    cin >> n;
+    
+    const int maxSize = 100;
+    float arr[maxSize];
+    float sumModule = 0;
+    
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "Элемент " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    
+    cout << "Введенный массив: ";
+    for (int i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    float sum = 0;
+    int lastNeg = 0;
+    int lastNegI = 0;
+
+    for (int i = 0; i < n; i++) {
+        
+        if (lastNeg > arr[i])
+        {
+            lastNegI = i;
+        }
+    }
+    for (int i = lastNegI + 1; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    cout << "Сумма целых частей элементов после последнего отрицательного элемента: " << sum << endl;
+
+    float C;
+    cout << "Введите значение C: ";
+    cin >> C;
+
+    float count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < C)
+        {
+            count++;
+        }
+    }
+    cout << "Количество элементов массива, меньших " << C << ": " << count << endl;
+
+    float maxElement = *max_element(arr, arr + n);
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        if (arr[i] >= maxElement * 0.8)
+        {
+            i++;
+        }
+        else if (arr[j] < maxElement * 0.8)
+        {
+            j--;
+        }
+        else
+        {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+
+    cout << "Преобразованный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
