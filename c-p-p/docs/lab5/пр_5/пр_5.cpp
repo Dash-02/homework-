91f386b8c60013a6148e53045efcce4f6b090dab
Dash-02 <dasha.semivolos@gmail.com>
2024-02-25 18:59:12 +0300

    cisco-lab-5-8-9

--- c-p-p/docs/lab5/пр_5/пр_5.cpp
+++ c-p-p/docs/lab5/пр_5/пр_5.cpp
@@ -1,4 +1,71 @@
-﻿//#include <iostream>
+﻿////#include <iostream>
+////#include <cmath>
+////#include <algorithm>
+////using namespace std;
+////
+////int main() {
+////    setlocale(LC_ALL, "Ru");
+////
+////    int n;
+////    float a, b;
+////    cout << "Введите длину массива: ";
+////    cin >> n;
+////
+////    const int maxSize = 100;
+////    float arr[maxSize];
+////    float sumModule = 0;
+////
+////    for (int i = 0; i < n; i++) {
+////        cout << "Элемент " << i + 1 << ": ";
+////        cin >> arr[i];
+////    }
+////
+////    cout << "Введенный массив: ";
+////    for (int i = 0; i < n; i++) {
+////        cout << arr[i] << " ";
+////    }
+////    cout << endl;
+////
+////    cout << "Введите интервал [a, b], a: ";
+////    cin >> a;
+////    cout << "b: ";
+////    cin >> b;
+////
+////    float count = 0;
+////    for (int i = 0; i < n; i++) {
+////        if (arr[i] >= a && arr[i] <= b) {
+////            count++;
+////        }
+////    }
+////    cout << "Количество элементов в диапазоне от " << a << " до " << b << ": " << count << endl;
+////    
+////    float maxElement = arr[0]; 
+////    int maxIndex = 0; 
+////    float sumModulus = 0; 
+////    for (int i = 0; i < n; i++) {
+////        if (arr[i] > maxElement) {
+////            maxElement = arr[i];
+////            maxIndex = i;
+////        }
+////        else if (i > maxIndex) { 
+////            sumModulus += fabs(arr[i]);
+////        }
+////    }
+////    cout << "Сумма модулей элементов после максимального элемента: " << sumModulus << endl;
+////    
+////    sort(arr, arr + n, [](float a, float b) {
+////        return fabs(a) > fabs(b);
+////        });
+////
+////    cout << "Отсортированный массив: ";
+////    for (int i = 0; i < n; i++) {
+////        cout << arr[i] << " ";
+////    }
+////    
+////    return 0;
+////}
+//
+//#include <iostream>
 //#include <cmath>
 //#include <algorithm>
 //using namespace std;
@@ -7,151 +74,150 @@
 //    setlocale(LC_ALL, "Ru");
 //
 //    int n;
-//    float a, b;
-//    cout << "Введите длину массива: ";
+//    cout << "Введите длину массива: "; 
 //    cin >> n;
-//
+//    
 //    const int maxSize = 100;
 //    float arr[maxSize];
 //    float sumModule = 0;
-//
-//    for (int i = 0; i < n; i++) {
+//    
+//    for (int i = 0; i < n; i++) 
+//    {
 //        cout << "Элемент " << i + 1 << ": ";
 //        cin >> arr[i];
 //    }
-//
+//    
 //    cout << "Введенный массив: ";
+//    for (int i = 0; i < n; i++) 
+//    {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    float sum = 0;
+//    int lastNeg = 0;
+//    int lastNegI = 0;
+//
+//    for (int i = 0; i < n; i++) {
+//        
+//        if (lastNeg > arr[i])
+//        {
+//            lastNegI = i;
+//        }
+//    }
+//    for (int i = lastNegI + 1; i < n; i++)
+//    {
+//        sum += arr[i];
+//    }
+//    cout << "Сумма целых частей элементов после последнего отрицательного элемента: " << sum << endl;
+//
+//    float C;
+//    cout << "Введите значение C: ";
+//    cin >> C;
+//
+//    float count = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (arr[i] < C)
+//        {
+//            count++;
+//        }
+//    }
+//    cout << "Количество элементов массива, меньших " << C << ": " << count << endl;
+//
+//    float maxElement = *max_element(arr, arr + n);
+//    int i = 0;
+//    int j = n - 1;
+//    while (i < j)
+//    {
+//        if (arr[i] >= maxElement * 0.8)
+//        {
+//            i++;
+//        }
+//        else if (arr[j] < maxElement * 0.8)
+//        {
+//            j--;
+//        }
+//        else
+//        {
+//            swap(arr[i], arr[j]);
+//            i++;
+//            j--;
+//        }
+//    }
+//
+//    cout << "Преобразованный массив: ";
 //    for (int i = 0; i < n; i++) {
 //        cout << arr[i] << " ";
 //    }
 //    cout << endl;
 //
-//    cout << "Введите интервал [a, b], a: ";
-//    cin >> a;
-//    cout << "b: ";
-//    cin >> b;
-//
-//    float count = 0;
-//    for (int i = 0; i < n; i++) {
-//        if (arr[i] >= a && arr[i] <= b) {
-//            count++;
-//        }
-//    }
-//    cout << "Количество элементов в диапазоне от " << a << " до " << b << ": " << count << endl;
-//    
-//    float maxElement = arr[0]; 
-//    int maxIndex = 0; 
-//    float sumModulus = 0; 
-//    for (int i = 0; i < n; i++) {
-//        if (arr[i] > maxElement) {
-//            maxElement = arr[i];
-//            maxIndex = i;
-//        }
-//        else if (i > maxIndex) { 
-//            sumModulus += fabs(arr[i]);
-//        }
-//    }
-//    cout << "Сумма модулей элементов после максимального элемента: " << sumModulus << endl;
-//    
-//    sort(arr, arr + n, [](float a, float b) {
-//        return fabs(a) > fabs(b);
-//        });
-//
-//    cout << "Отсортированный массив: ";
-//    for (int i = 0; i < n; i++) {
-//        cout << arr[i] << " ";
-//    }
-//    
 //    return 0;
 //}
+//
+// task 5-24
 
 #include <iostream>
 #include <cmath>
-#include <algorithm>
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Ru");
+    int N;
+    cout << "Введите размер массива: ";
+    cin >> N;
 
-    int n;
-    cout << "Введите длину массива: "; 
-    cin >> n;
-    
-    const int maxSize = 100;
-    float arr[maxSize];
-    float sumModule = 0;
-    
-    for (int i = 0; i < n; i++) 
-    {
-        cout << "Элемент " << i + 1 << ": ";
+    double* arr = new double[N];
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
-    
-    cout << "Введенный массив: ";
-    for (int i = 0; i < n; i++) 
-    {
+
+    double sum = 0;
+    double product = 1;
+    int firstNegIndex = -1;
+    int lastNegIndex = -1;
+
+    for (int i = 0; i < N; i++) {
+        if (i % 2 != 0) {
+            sum += arr[i];
+        }
+
+        if (arr[i] < 0) {
+            if (firstNegIndex == -1) {
+                firstNegIndex = i;
+            }
+            lastNegIndex = i;
+        }
+    }
+
+    if (firstNegIndex != -1 && lastNegIndex != -1 && firstNegIndex != lastNegIndex) {
+        for (int i = firstNegIndex + 1; i < lastNegIndex; i++) {
+            product *= arr[i];
+        }
+    }
+
+    int newArraySize = 0;
+    for (int i = 0; i < N; i++) {
+        if (abs(arr[i]) > 1) {
+            arr[newArraySize] = arr[i];
+            newArraySize++;
+        }
+    }
+
+    for (int i = newArraySize; i < N; i++) {
+        arr[i] = 0;
+    }
+
+    cout << "Сумма элементов с нечетными номерами: " << sum << endl;
+    cout << "Произведение элементов между первым и последним отрицательными элементами: " << product << endl;
+    cout << "Сжатый массив: ";
+    for (int i = 0; i < newArraySize; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 
-    float sum = 0;
-    int lastNeg = 0;
-    int lastNegI = 0;
-
-    for (int i = 0; i < n; i++) {
-        
-        if (lastNeg > arr[i])
-        {
-            lastNegI = i;
-        }
-    }
-    for (int i = lastNegI + 1; i < n; i++)
-    {
-        sum += arr[i];
-    }
-    cout << "Сумма целых частей элементов после последнего отрицательного элемента: " << sum << endl;
-
-    float C;
-    cout << "Введите значение C: ";
-    cin >> C;
-
-    float count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] < C)
-        {
-            count++;
-        }
-    }
-    cout << "Количество элементов массива, меньших " << C << ": " << count << endl;
-
-    float maxElement = *max_element(arr, arr + n);
-    int i = 0;
-    int j = n - 1;
-    while (i < j)
-    {
-        if (arr[i] >= maxElement * 0.8)
-        {
-            i++;
-        }
-        else if (arr[j] < maxElement * 0.8)
-        {
-            j--;
-        }
-        else
-        {
-            swap(arr[i], arr[j]);
-            i++;
-            j--;
-        }
-    }
-
-    cout << "Преобразованный массив: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    delete[] arr;
 
     return 0;
 }
-
---
+++ c-p-p/ср/sr_Seidaliev_Semivolos_5/sr_Seidaliev_Semivolos_5/sr_Seidaliev_Semivolos_5.cpp
@@ -0,0 +1,182 @@
+﻿// task-1
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//double funcY(double x) {
+//    double result = 0;
+//    for (int n = 1; n <= 20; ++n) {
+//        result += (pow(x, 2 * n - 2) / (4 * cos(n * pow(x, 2))));
+//    }
+//    return result;
+//}
+//
+//int main() {
+//    double a = 0.1;
+//    double b = 1.2;
+//    double h = 0.1;
+//
+//    cout << "x\t\ty(x)" << endl;
+//    for (double x = a; x <= b; x += h) {
+//        double y = funcY(x);
+//        cout << x << "\t\t" << y << endl;
+//    }
+//
+//    return 0;
+//}
+// --------------- task-2
+//
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//
+//double funcRecY(double x, int n) {
+//    if (n == 0) {
+//        return 0;
+//    }
+//    return funcRecY(x, n - 1) + (pow(x, 2 * n - 2) / (4 * cos(n * pow(x, 2))));
+//}
+//
+//int main() {
+//    double a = 0.1;
+//    double b = 1.2;
+//    double h = 0.1;
+//
+//    cout << "x\t\ty(x)" << endl;
+//    for (double x = a; x <= b; x += h) {
+//        double y = funcRecY(x, 20);
+//        cout << x << "\t\t" << y << endl;
+//    }
+//
+//    return 0;
+//}
+ 
+//task-
+//
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//double funcY(double x) {
+//        double result = 0;
+//        for (int n = 1; n <= 20; ++n) {
+//            result += (1 + pow(x, n - 1)) / fabs(pow(sin(x), n));
+//        }
+//        return result;
+//    }
+//    
+//    int main() {
+//        double a = 0.1;
+//        double b = 1.2;
+//        double h = 0.1;
+//    
+//        cout << "x\t\ty(x)" << endl;
+//        for (double x = a; x <= b; x += h) {
+//            double y = funcY(x);
+//            cout << x << "\t\t" << y << endl;
+//        }
+//    
+//        return 0;
+//    }
+//
+//
+//#include <iostream>
+//#include <cmath>
+//
+//using namespace std;
+//double funcY(double x) {
+//    double result = 0;
+//    for (int n = 1; n <= 20; ++n) {
+//        result += (1 + pow(x, n - 1)) / fabs(pow(sin(x), n));
+//    }
+//    return result;
+//}
+//
+//int main() {
+//    double a = 0.1;
+//    double b = 1.2;
+//    double h = 0.1;
+//
+//    cout << "x\t\ty(x)" << endl;
+//    for (double x = a; x <= b; x += h) {
+//        double y = funcY(x);
+//        cout << x << "\t\t" << y << endl;
+//    }
+//
+//    return 0;
+//}
+
+
+//task 3-1
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void findIncreasingNumbers(int currentNumber, int n, int lastDigit, vector<int>& result) {
+    if (n == 0) {
+        result.push_back(currentNumber);
+        return;
+    }
+    if (lastDigit == 0 || lastDigit == 9) {
+        return;
+    }
+    int newNumber1 = currentNumber * 10 + lastDigit + 1;
+    findIncreasingNumbers(newNumber1, n - 1, lastDigit + 1, result);
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество цифр (n): ";
+    cin >> n;
+
+    vector<int> result;
+    for (int digit = 1; digit <= 8; ++digit) {
+        findIncreasingNumbers(digit, n - 1, digit, result);
+    }
+
+    cout << "Все натуральные " << n << "-значные числа с возрастающей последовательностью цифр:" << endl;
+    for (int number : result) {
+        cout << number << endl;
+    }
+
+    return 0;
+}
+
+
+//task 3-2
+
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int sumOfDigits(int number) {
+//    int sum = 0;
+//    while (number > 0) {
+//        sum += number % 10;
+//        number /= 10;
+//    }
+//    return sum;
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "RU");
+//
+//    int input;
+//    std::cout << "Введите число: ";
+//    std::cin >> input;
+//
+//    int count = 0;
+//    while (input != 0) {
+//        input -= sumOfDigits(input);
+//        count++;
+//    }
+//
+//    std::cout << "Число действий: " << count << std::endl;
+//
+//    return 0;
+//}
